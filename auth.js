cbf6c0f083d6d1a86c174725b599a40c1fc42a13
Balint Csikos <csikos.balint@fnf.hu>
2020-12-06 12:58:13 +0100

    working via CognitoIdentity but no session name

--- auth.js
+++ auth.js
@@ -1,14 +1,13 @@
-let identityPool = 'us-east-1:XXXXXXXXXXXXXXXXXXXXXXXXXXXXX'; //Cognito Identity Pool
-let region = 'us-east-1';
-let poolId = 'us-east-1_XXXXXXXX'; //Cognito User Pool
-let clientId = 'XXXXXXXXXXXXXXXXX'; //Cognito User Pool App
-let appDomain = 'XXXXXX.auth.us-east-1.amazoncognito.com';
-let endpoint = "https://XXXXXXXXXXX.execute-api.us-east-1.amazonaws.com/prod"; //API Endpoint URL
+let identityPool = 'eu-west-1:0db40126-6908-4973-99fc-e2580be09ede'; //Cognito Identity Pool
+let region = 'eu-west-1';
+let poolId = 'eu-west-1_bOuCRBNUl'; //Cognito User Pool
+let clientId = '3utvtepv1cbl53pn8bidpd0cnt'; //Cognito User Pool App
+let appDomain = '327953370525-qs.auth.eu-west-1.amazoncognito.com';
+let endpoint = 'https://7gn67vo4bk.execute-api.eu-west-1.amazonaws.com/prod'; //API Endpoint URL
 let authData = {
-    ClientId : clientId, 
-    AppWebDomain : appDomain,
-    TokenScopesArray : ['openid'],
-    RedirectUriSignIn : 'https://d12345example.cloudfront.net', //CloudFront Distribution URL
-    RedirectUriSignOut : 'https://d12345example.cloudfront.net' //CloudFront Distribution URL
+  ClientId: clientId,
+  AppWebDomain: appDomain,
+  TokenScopesArray: ['openid'],
+  RedirectUriSignIn: 'https://dij0nouyfvoue.cloudfront.net', //CloudFront Distribution URL
+  RedirectUriSignOut: 'https://dij0nouyfvoue.cloudfront.net', //CloudFront Distribution URL
 };
-
